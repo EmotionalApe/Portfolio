5e47216991cff16c6c1c4575643b4339a7b0efa9
EmotionalApe <maruf1611khan@gmail.com>
2023-05-31 14:33:16 +0530

    added links and responsiveness

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,18 @@
-import React, { useRef } from 'react'
+import React  from 'react'
+import { useRef } from 'react'
 import './Contact.css'
 import Phone from '../../images/phone.svg'
 import Address from '../../images/address.svg'
 import Email from '../../images/email.svg'
+import Download from '../../images/download.svg'
+import Github from '../../images/github.svg'
+import LinkedIn from '../../images/linkedin.svg'
 
 function Contact() {
     const formRef = useRef()
-    // const handleSubmit = (event) {
-    //     event.preventDefault()
-    // }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
   return (
     <div className='c' id="contact">
         <div className="cBg"></div>
@@ -17,13 +21,23 @@ function Contact() {
                 <h1 className="cTitle">Contact me!</h1>
                 <div className="cInfo">
                     <div className="cInfoItem">
-                        <img src={Phone} alt="" className="cIcon" /> +91-8668239228
+                        <img src={Download} alt="" className="cIcon" /> <a target="_blank" rel='noreferrer' href='https://drive.google.com/file/d/1D3bP1vS1K1mqzQSXmxspmtTnijw9-_tY/view?usp=sharing'>Download Resume</a>
                     </div>
                     <div className="cInfoItem">
-                        <img src={Email} alt="" className="cIcon" /> maruf1611khan@gmail.com
+                        <img src={Github} alt="" className="cIcon" /> <a target="_blank" rel='noreferrer' href='https://github.com/EmotionalApe'>Github profile</a>  
                     </div>
                     <div className="cInfoItem">
-                        <img src={Address} alt="" className="cIcon" /> A505 Fortune Srushti, Yewalewadi, Pune-48
+                        <img src={LinkedIn} alt="" className="cIcon" /> <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/maruf-j-khan'>LinkedIn profile</a> 
+                    </div>
+                    <div className="cInfoItem">
+                        <img src={Phone} alt="" className="cIcon" /> <a href="tel:+918668239228">+91-8668239228</a>
+                    </div>
+                    <div className="cInfoItem">
+                        <img src={Email} alt="" className="cIcon" /> <a href="mailto:maruf1611khan@gmail.com">maruf1611khan@gmail.com</a>
+
+                    </div>
+                    <div className="cInfoItem">
+                        <img src={Address} alt="" className="cIcon" /> <a target="_blank" rel='noreferrer' href='https://goo.gl/maps/khCMhrnAAdgywFz27'>A505 Fortune Srushti, Yewalewadi, Pune-48</a> 
                     </div>
                 </div>
             </div>
